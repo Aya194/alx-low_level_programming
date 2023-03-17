6b4c8d604ea7a63b12d780290ff75271cb744943
Aya Nady <ayaallah.nady17@stemmaadi.edu.eg>
2023-03-16 19:16:31 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Function to print text with puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
