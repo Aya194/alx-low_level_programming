84f2f8e5e17e07a3f8dabc02851c312a49419220
Aya Nady <ayaallah.nady17@stemmaadi.edu.eg>
2023-03-17 10:02:51 -0700

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 122)
+
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
